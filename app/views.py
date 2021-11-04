a3f5639102a5884207965da12cf54761b40e54de
chenhongGitHub <e0709248@u.nus.edu>
2021-11-04 21:11:50 +0800

    added cols as per question 1b

--- app/views.py
+++ app/views.py
@@ -11,6 +11,9 @@ from plotly.subplots import make_subplots
 from app.utils import namedtuplefetchall, clamp
 from app.forms import ImoForm
 
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 PAGE_SIZE = 20
 COLUMNS = [
     'imo',
@@ -32,11 +35,9 @@ COLUMNS2 = [
 COLUMNS3 = [
     'ship_id',
     'verifier_id',
+    'date_id',
     'eedi',
-    'report_period',
     'port_regist',
-    'issue_date',
-    'expiry_date',
     'total_fuel_consmp',
     'total_co2',
     'total_time_sea',
@@ -45,7 +46,10 @@ COLUMNS3 = [
 ]
 
 COLUMNS4 = [
-    'verifier_id'
+    'ship_id',
+    'imo',
+    'ship_name',
+    'ship_type',   
 ]
 
 COLUMNS5 = [
@@ -56,6 +60,16 @@ COLUMNS5 = [
     'verifier_city',
     'accredition_no',
     'verifier_country'
+]
+
+COLUMNS6= [
+    'date_id',
+    'date',
+    'week',
+    'month',
+    'quarter',
+    'year_half',
+    'year'
 ]
 
 
@@ -255,8 +269,8 @@ def visual(request):
  
     with connections['default'].cursor() as cursor:
         cursor.execute('select count(distinct c.imo), c.ship_type, min(c.technical_efficiency_number), avg(c.technical_efficiency_number), max(c.technical_efficiency_number) from co2emission_reduced as c group by c.ship_type;')
-        
         rows = cursor.fetchall() #if this is here, indented, then its fine
+
     #print(str(rows))
     # Generating some data for plots.
     li_avg=[]
@@ -306,8 +320,92 @@ def visual(request):
     plot_div2 = plot({'data': fig2, 'layout': layout2}, 
                     output_type='div')
 
+    #new part for the group project***********************************************
+    with connections['default'].cursor() as cursor:
+        cursor.execute('select f.total_co2, f.total_time_sea, f.total_fuel_consmp from fact as f;')
+        rows2 = cursor.fetchall() #if this is here, indented, then its fine
+
+    co2_li=[]
+    tts_li=[]
+    tfc_li=[]
+    for i in range(len(rows2)):
+        co2_li.append(rows2[i][0])    
+        tts_li.append(rows2[i][1]) 
+        tfc_li.append(rows2[i][2])
+
+    fig3 = go.Scatter(x=np.log10(tts_li),y=np.log10(co2_li), mode='markers') 
+    fig4 = go.Scatter(x=np.log10(tts_li),y=np.log10(tfc_li), mode='markers') 
+    
+    lr=LinearRegression().fit(np.log10(tts_li).reshape(-1,1),np.log10(co2_li).reshape(-1,1))
+    lr_y_pred = lr.predict(np.log10(tts_li).reshape(-1,1))  
+    lr_y_pred_li=[]
+    for i in range(len(lr_y_pred)):
+        lr_y_pred_li.append(lr_y_pred[i][0])
+
+    fig3_lr= go.Scatter(x=np.log10(tts_li),y=lr_y_pred_li,line=dict(color='firebrick', width=4))  
+
+    lr2=LinearRegression().fit(np.log10(tts_li).reshape(-1,1),np.log10(tfc_li).reshape(-1,1))
+    lr2_y_pred = lr2.predict(np.log10(tts_li).reshape(-1,1))  
+    lr2_y_pred_li=[]
+    for i in range(len(lr2_y_pred)):
+        lr2_y_pred_li.append(lr2_y_pred[i][0])
+    fig4_lr= go.Scatter(x=np.log10(tts_li),y=lr2_y_pred_li,line=dict(color='firebrick', width=4))  
+
+    layout3 = {
+        'title': 'Log_10(Total time at sea) versus Log_10(Total Co2)',
+        #'title': str(lr2_y_pred_li),
+        'yaxis_title': 'Log_10(Total Co2)',
+        'xaxis_title': 'Log_10(Total time at sea)',
+        'height': 620,
+        'width': 560,
+    }
+    layout4 = {
+        'title': 'Total time at sea versus Total Fuel Consumption',
+        'yaxis_title': 'Total Fuel Consumption)',
+        'xaxis_title': 'Total time at sea',
+        'height': 620,
+        'width': 560,
+    }
+    plot_div3 = plot({'data':[fig3, fig3_lr], 'layout': layout3}, 
+                    output_type='div')
+    plot_div4 = plot({'data': [fig4, fig4_lr], 'layout': layout4}, 
+                    output_type='div')
+
+    with connections['default'].cursor() as cursor:
+        cursor.execute('select avg(f.total_co2), avg(f.total_time_sea), s.ship_type from fact as f, ship_dim as s where f.ship_id = s.ship_id group by s.ship_type;')
+        rows3 = cursor.fetchall() #if this is here, indented, then its fine
+    
+    avg_co2_li=[]
+    avg_tts_li=[]
+    ship_type_li=[]
+    for i in range(len(rows3)):
+        avg_co2_li.append(rows3[i][0])    
+        avg_tts_li.append(rows3[i][1]) 
+        ship_type_li.append(rows3[i][2])
+
+    fig5 = go.Bar(x=ship_type_li,y=avg_co2_li) 
+    fig6 = go.Pie(labels=ship_type_li,values=avg_tts_li) 
+
+    layout5 = {
+        'title': 'average total co2 bar chart by ship type',
+        'yaxis_title': 'average total co2',
+        'xaxis_title': 'ship type',
+        'height': 620,
+        'width': 560,
+    }
+    layout6 = {
+        'title': 'average total fuel consumption pie chart by ship type',
+        'yaxis_title': 'Total Fuel Consumption)',
+        'xaxis_title': 'Total time at sea',
+        'height': 620,
+        'width': 560,
+    }
+    plot_div5 = plot({'data': fig5, 'layout': layout5}, 
+                    output_type='div')
+    plot_div6 = plot({'data': fig6, 'layout': layout6}, 
+                    output_type='div')
     return render(request, 'visual.html', 
-                  context={'plot_div': plot_div,'plot_div2':plot_div2,'nbar': 'visual'})
+                  context={'plot_div': plot_div,'plot_div2':plot_div2,'plot_div3':plot_div3, 'plot_div4':plot_div4, 'plot_div5':plot_div5, 'plot_div6':plot_div6, 'nbar': 'visual'})
 
 
 
@@ -364,7 +462,7 @@ def ship_dim(request, page=1):
         offset = (page - 1) * PAGE_SIZE
         cursor.execute(f'''
             SELECT {", ".join(COLUMNS4)}
-            FROM fact
+            FROM ship_dim
 	    ORDER BY {order_by}
             OFFSET %s
             LIMIT %s
@@ -387,11 +485,171 @@ def ship_dim(request, page=1):
 
 
 
+def adv_q_visual(request):
+    """ 
+    View demonstrating how to display a graph object
+    on a web page with Plotly. 
+    """
+    
+    #cursor = conn.cursor()    
+ 
+    with connections['default'].cursor() as cursor:
+        cursor.execute('SELECT d.Year, s.ship_type,ROUND(PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY f.eedi ASC)::NUMERIC, 2) AS percentile_25, ROUND(PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY f.eedi ASC)::NUMERIC, 2) AS percentile_50, ROUND(PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY f.eedi ASC)::NUMERIC, 2) AS percentile_75, ROUND(PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY f.eedi ASC)::NUMERIC, 2) AS percentile_95 FROM fact f, ship_dim s, date_dim d WHERE f.ship_id= s.ship_id AND d.date_id = f.date_id GROUP BY ROLLUP(d.Year, s.ship_type);')
+        rows = cursor.fetchall() #if this is here, indented, then its fine
+
+    #print(str(rows))
+    # Generating some data for plots.
+    bulkcarrier_25pc_li=[]
+    bulkcarrier_50pc_li=[]
+    bulkcarrier_75pc_li=[]
+    bulkcarrier_95pc_li=[]
+
+    containership_25pc_li=[]
+    containership_50pc_li=[]
+    containership_75pc_li=[]
+    containership_95pc_li=[]
+
+    year_li=[2019,2020,2021]
+    
+    bulkcarrier_25pc_li.append(rows[0][2])
+    bulkcarrier_25pc_li.append(rows[9][2])
+    bulkcarrier_25pc_li.append(rows[18][2])
+
+    bulkcarrier_50pc_li.append(rows[0][3])
+    bulkcarrier_50pc_li.append(rows[9][3])
+    bulkcarrier_50pc_li.append(rows[18][3])
+
+    bulkcarrier_75pc_li.append(rows[0][4])
+    bulkcarrier_75pc_li.append(rows[9][4])
+    bulkcarrier_75pc_li.append(rows[18][4])
+
+    bulkcarrier_95pc_li.append(rows[3][5])
+    bulkcarrier_95pc_li.append(rows[12][5])
+    bulkcarrier_95pc_li.append(rows[22][5])
+
+    containership_25pc_li.append(rows[3][2])
+    containership_25pc_li.append(rows[12][2])
+    containership_25pc_li.append(rows[22][2])
+
+    containership_50pc_li.append(rows[3][3])
+    containership_50pc_li.append(rows[12][3])
+    containership_50pc_li.append(rows[22][3])
+
+    containership_75pc_li.append(rows[3][4])
+    containership_75pc_li.append(rows[12][4])
+    containership_75pc_li.append(rows[22][4])
+
+    containership_95pc_li.append(rows[3][5])
+    containership_95pc_li.append(rows[12][5])
+    containership_95pc_li.append(rows[22][5])
+
+
+    
+    fig1a = go.Scatter(x=year_li,y=bulkcarrier_25pc_li, name='Bulk carrier 25th percentile') 
+    fig1b = go.Scatter(x=year_li,y=bulkcarrier_50pc_li, name='Bulk carrier 50th percentile') 
+    fig1c = go.Scatter(x=year_li,y=bulkcarrier_75pc_li, name='Bulk carrier 75th percentile') 
+    fig1d = go.Scatter(x=year_li,y=bulkcarrier_95pc_li, name='Bulk carrier 95th percentile') 
+
+    fig1e = go.Scatter(x=year_li,y=containership_25pc_li, name='container ship 25th percentile') 
+    fig1f = go.Scatter(x=year_li,y=containership_50pc_li, name='container ship 50th percentile') 
+    fig1g = go.Scatter(x=year_li,y=containership_75pc_li, name='container ship 75th percentile') 
+    fig1h = go.Scatter(x=year_li,y=containership_95pc_li, name='container ship 95th percentile')  
+
+    # Setting layout of the figure.
+    layout = {
+        'title': 'Bulk carrier/ container ship eedi 25, 50, 75, and  95th percentile by year'  ,
+        'xaxis_title': 'year',
+        'yaxis_title': 'EEDI',
+        'height': 620,
+        'width': 560,
+    }
+
+
+    # Getting HTML needed to render the plot.
+    plot_div = plot({'data': [fig1a,fig1b, fig1c, fig1d,fig1e,fig1f,fig1g,fig1h], 'layout': layout}, 
+                    output_type='div')
+
+#************** second advanced query visualization ************************
+    with connections['default'].cursor() as cursor:
+        cursor.execute('SELECT rank_filter.* FROM (SELECT s.ship_name, s.ship_type, f.eedi,RANK() OVER (PARTITION BY s.ship_type ORDER BY f.eedi) FROM fact f, ship_dim s, date_dim d WHERE f.ship_id= s.ship_id AND d.date_id = f.date_id AND d.Year = 2021 ) rank_filter WHERE RANK <=3;')
+        rows2 = cursor.fetchall() #if this is here, indented, then its fine
+    
+    name_li=[]
+    eedi_li=[]
+    type_li=[]
+
+    for i in range(len(rows2)):
+        name_li.append(rows2[i][0])
+        eedi_li.append(rows2[i][2])    
+        type_li.append(rows2[i][1])
+
+    fig2a=go.Bar(x=name_li[0:3],y=eedi_li[0:3],name=type_li[0])  
+    fig2b=go.Bar(x=name_li[3:6],y=eedi_li[3:6],name=type_li[4]) 
+    fig2c=go.Bar(x=[name_li[6]],y=[eedi_li[6]],name=type_li[6]) 
+    fig2d=go.Bar(x=name_li[7:10],y=eedi_li[7:10],name=type_li[8]) 
+    fig2e=go.Bar(x=name_li[10:13],y=eedi_li[10:13],name=type_li[10]) 
+    fig2f=go.Bar(x=name_li[13:16],y=eedi_li[13:16],name=type_li[13]) 
+    fig2g=go.Bar(x=name_li[16:19],y=eedi_li[16:19],name=type_li[16]) 
+    fig2h=go.Bar(x=name_li[19:21],y=eedi_li[19:21],name=type_li[19]) 
+    fig2i=go.Bar(x=[name_li[21]],y=[eedi_li[21]],name=type_li[21]) 
+
+    layout2 = {
+        'title': 'top 3 lowest eedi ships from each ship category'  ,
+        'xaxis_title': 'ship name',
+        'yaxis_title': 'EEDI',
+        'height': 620,
+        'width': 700,
+    }  
+    plot_div2=plot({'data': [fig2a,fig2b,fig2c,fig2d,fig2e,fig2f,fig2g,fig2h,fig2i], 'layout': layout2}, 
+                    output_type='div')
+
+#********************** do the third advanced query here
+    with connections['default'].cursor() as cursor:
+        cursor.execute('SELECT rank_filter.* FROM ( SELECT s.ship_name, s.ship_type, sum_f.avg_ship, sum_f.avg_time, RANK() OVER ship_time AS time_rank, ROUND(AVG(sum_f.avg_ship) OVER ship_eedi::NUMERIC, 2) AS avg_type FROM ship_dim s, (SELECT f1.ship_id, ROUND(AVG(f1.total_time_sea)::NUMERIC, 2) AS avg_time, ROUND(AVG(f1.eedi)::NUMERIC, 2) AS avg_ship FROM fact f1 GROUP BY f1.ship_id) sum_f WHERE sum_f.ship_id= s.ship_id WINDOW ship_time AS (PARTITION BY  s.ship_type ORDER BY (sum_f.avg_time) DESC), ship_eedi AS (PARTITION BY  s.ship_type ORDER BY (sum_f.avg_ship) ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) ORDER BY s.ship_type) rank_filter WHERE time_rank<=3;')
+        rows3 = cursor.fetchall() #if this is here, indented, then its fine
+    
+    name_li2=[]
+    eedi_li2=[]
+    type_li2=[]
+
+    for i in range(len(rows3)):
+        name_li2.append(rows3[i][0])
+        eedi_li2.append(rows3[i][2])    
+        type_li2.append(rows3[i][1])
+
+    fig3a=go.Bar(x=name_li2[0:3],y=eedi_li2[0:3],name=type_li2[0])  
+    fig3b=go.Bar(x=name_li2[3:6],y=eedi_li2[3:6],name=type_li2[4]) 
+    fig3c=go.Bar(x=[name_li2[6]],y=[eedi_li2[6]],name=type_li2[6]) 
+    fig3d=go.Bar(x=name_li2[7:10],y=eedi_li2[7:10],name=type_li2[8]) 
+    fig3e=go.Bar(x=name_li2[10:13],y=eedi_li2[10:13],name=type_li2[10]) 
+    fig3f=go.Bar(x=name_li2[13:16],y=eedi_li2[13:16],name=type_li2[13]) 
+    fig3g=go.Bar(x=name_li2[16:19],y=eedi_li2[16:19],name=type_li2[16]) 
+    fig3h=go.Bar(x=name_li2[19:21],y=eedi_li2[19:21],name=type_li2[19]) 
+    fig3i=go.Bar(x=[name_li2[21]],y=[eedi_li2[21]],name=type_li2[21]) 
+    fig3j=go.Bar(x=[name_li2[22]],y=[eedi_li2[22]],name=type_li2[22]) 
+
+    layout3 = {
+        'title': 'top 3 highest eedi ships from each ship category'  ,
+        'xaxis_title': 'ship name',
+        'yaxis_title': 'EEDI',
+        'height': 620,
+        'width': 700,
+    }  
+    plot_div3=plot({'data': [fig3a,fig3b,fig3c,fig3d,fig3e,fig3f,fig3g,fig3h,fig3i,fig3j], 'layout': layout3}, 
+                    output_type='div')   
+
+   
+    return render(request, 'adv_q_visual.html', 
+                  context={'plot_div': plot_div, 'plot_div2': plot_div2,'plot_div3': plot_div3,'nbar': 'adv_q_visual'})
+
+
+
+
 def verifier_dim(request, page=1):
     """Shows the verifier_dim table page"""
     msg = None
     order_by = request.GET.get('order_by', '')
-    order_by = order_by if order_by in COLUMNS5 else 'ship_id'
+    order_by = order_by if order_by in COLUMNS5 else 'verifier_id'
 
     with connections['default'].cursor() as cursor:
         cursor.execute('SELECT COUNT(*) FROM verifier_dim')
@@ -401,8 +659,8 @@ def verifier_dim(request, page=1):
 
         offset = (page - 1) * PAGE_SIZE
         cursor.execute(f'''
-            SELECT {", ".join(COLUMNS3)}
-            FROM fact
+            SELECT {", ".join(COLUMNS5)}
+            FROM verifier_dim
 	    ORDER BY {order_by}
             OFFSET %s
             LIMIT %s
@@ -423,3 +681,39 @@ def verifier_dim(request, page=1):
     }
     return render(request, 'verifier_dim.html', context)
 
+
+def date_dim(request, page=1):
+    """Shows the date_dim table page"""
+    msg = None
+    order_by = request.GET.get('order_by', '')
+    order_by = order_by if order_by in COLUMNS6 else 'date_id'
+
+    with connections['default'].cursor() as cursor:
+        cursor.execute('SELECT COUNT(*) FROM verifier_dim')
+        count = cursor.fetchone()[0]
+        num_pages = (count - 1) // PAGE_SIZE + 1
+        page = clamp(page, 1, num_pages)
+
+        offset = (page - 1) * PAGE_SIZE
+        cursor.execute(f'''
+            SELECT {", ".join(COLUMNS6)}
+            FROM date_dim
+	    ORDER BY {order_by}
+            OFFSET %s
+            LIMIT %s
+        ''', [offset, PAGE_SIZE])
+        rows = namedtuplefetchall(cursor)
+
+    imo_deleted = request.GET.get('deleted', False)
+    if imo_deleted:
+        msg = f'✔ IMO {imo_deleted} deleted'
+
+    context = {
+        'nbar': 'date_dim',
+        'page': page,
+        'rows': rows,
+        'num_pages': num_pages,
+        'msg': msg,
+        'order_by': order_by
+    }
+    return render(request, 'date_dim.html', context)
--- core/urls.py
+++ core/urls.py
@@ -18,12 +18,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('aggregation/', app.views.aggregation, name='aggregation'), 
     path('visual/', app.views.visual, name='visual'), 
+    path('adv_q_visual/', app.views.adv_q_visual, name='adv_q_visual'), 
     path('fact/', app.views.fact, name='fact'),
     path('fact/<int:page>', app.views.fact, name='fact'),
     path('ship_dim/', app.views.ship_dim, name='ship_dim'),
     path('ship_dim/<int:page>', app.views.ship_dim, name='ship_dim'),
     path('verifier_dim/', app.views.verifier_dim, name='verifier_dim'),
     path('verifier_dim/<int:page>', app.views.verifier_dim, name='verifier_dim'),
+    path('date_dim/', app.views.date_dim, name='date_dim'),
+    path('date_dim/<int:page>', app.views.date_dim, name='date_dim'),
 ]
 
 
